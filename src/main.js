c34a4d65235897cea0a2f7406f373c3e8075bdb5
Andrea030105 <tummoloandrea05@gmail.com>
2026-01-22 16:45:26 +0100

    import-bootstrap-axios

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+// Importa CSS di Bootstrap
+import axios from "axios";
+
+// Importa CSS di Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Importa JavaScript di Bootstrap (opzionale, solo se usi componenti interattivi)
+import * as bootstrap from "bootstrap/dist/js/bootstrap.bundle";
+
+const app = createApp(App);
+app.provide("bootstrap", bootstrap);
+app.mount("#app");
