29db59ec41b47ea6c38c19af25801f7c53181c89
Andrea030105 <tummoloandrea05@gmail.com>
2026-01-23 12:20:41 +0100

    header-done

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,8 +4,35 @@ export default {
 }
 </script>
 <template>
-    <div>
-        caio
-    </div>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="title-container d-flex align-items-center">
+                        <img src="../assets/images/yugioh_logo.png" alt="yugioh_logo">
+                        <h1>
+                            Yo-Gi-Oh API
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 </template>
-<style lang="scss"></style>
+<style lang="scss">
+@use '../styles/partial/variables' as *;
+
+header {
+    background-color: $whiteCl;
+
+    .title-container {
+        img {
+            width: 10%;
+        }
+
+        h1 {
+            margin-left: 2rem;
+        }
+    }
+}
+</style>
